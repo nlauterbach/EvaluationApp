07d7484da371ddd361455dc40bf3e1ddc873bda2
Nathan Lauterbach <lautien012@gmail.com>
2019-05-05 10:31:23 -0700

    Add project files.

---
+++ EvaluationApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EvaluationApp.Startup))]
+namespace EvaluationApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
